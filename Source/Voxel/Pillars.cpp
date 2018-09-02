e6f134113c37254ff9897e4701e1a5c1caf7ccda
TheKindlerofWildfires <redfarofket@gmail.com>
2018-09-02 18:30:51 -0500

    Magic numbers applied

--- Source/Voxel/Pillars.cpp
+++ Source/Voxel/Pillars.cpp
@@ -5,17 +5,17 @@
 Pillars::Pillars()
 {
 	land = FastNoise();
-	land.SetSeed(5);
+	land.SetSeed(6);
 	land.SetFrequency(0.01);
 	land.SetFractalOctaves(5);
 
 	overhangs = FastNoise();
-	overhangs.SetSeed(5);
+	overhangs.SetSeed(6);
 	overhangs.SetFrequency(0.1);
 	overhangs.SetFractalGain(0.1);
 
 	pillars = FastNoise();
-	pillars.SetSeed(5);
+	pillars.SetSeed(6);
 	pillars.SetFrequency(0.05);
 	pillars.SetCellularJitter(0.9);
 	folliage = 0.4;
@@ -36,42 +36,45 @@ int Pillars::Generate(int VoxelX, int VoxelY, int VoxelZ) {
 	return Value;
 }
 int Pillars::Land(int VoxelX, int VoxelY, int VoxelZ) {
+	int Value = 0;
 	if (VoxelZ < -50) {//Controls floor stop
-		return 2;
+		Value = 2;
 	}
 
 	float Mheight = land.GetPerlinFractal((VoxelX) * 1, (VoxelY) * 1);
-	Mheight *= 20;
+	
 	float pillar = pillars.GetCellular((VoxelX) * 1, (VoxelY) * 1);
 
-	if (pillar > 0.5) {
+	/*if (Mheight > 0.01 && pillar>0.2) {
 		pillar *= 50;
 	}
 	else {
 		pillar = 0;
-	}
+	}*/
+	pillar *= 50;
+	Mheight *= 25;
 
-	if (VoxelZ > Mheight && pillar != 0) {
-		if (overhangs.GetPerlinFractal(VoxelX, VoxelY, VoxelZ) <= 0.1) { //Pillar cuts
-			if (VoxelZ < (pillar - 2)) {
-				return 2;
+	if (VoxelZ > Mheight && pillar != 0) {//So my idea before I forget is to use this information to cut out select for at base
+		if (overhangs.GetPerlinFractal(VoxelX, VoxelY, VoxelZ) <= 0.2) { //Pillar cuts
+			if (VoxelZ < (pillar - 2)&& VoxelZ>(pow(std::abs(Mheight),2)*pillar/(pillar-5))) {//The abs is is you want to edit the 2
+				Value = 2;
 
 			}
-			else if (VoxelZ <= (pillar)) {
-				return 3;
+			else if (VoxelZ <= (pillar) && VoxelZ > (pow(std::abs(Mheight),2)*pillar/(pillar-5))) {
+				Value = 3;
 			}
 		}
-	}else if (overhangs.GetPerlinFractal(VoxelX, VoxelY, VoxelZ) <= 0.25) {//Land cave size
+	}else if (overhangs.GetPerlinFractal(VoxelX, VoxelY, VoxelZ) <= 0.35) {//Land cave size
 		if (VoxelZ < (Mheight - 2)) {
-			return 2;
+			Value = 2;
 
 		}
-		else if (VoxelZ <= (Mheight)) {
-			return 3;
+		else if (VoxelZ <= (Mheight)) {//pill = 0 makes dirt sorta fix
+			Value = 3;
 		}
 
 	}
-	return 0;
+	return Value;
 }
 int Pillars::Detail(int VoxelX, int VoxelY, int VoxelZ, int Value) {
 	if ((abs(overhangs.GetPerlin(VoxelX * 20, VoxelY * 20, VoxelZ * 20)) > folliage) && Value == 3) {
